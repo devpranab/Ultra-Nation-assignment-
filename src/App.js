32afba73c33efc653021ec04c741f7d53a189c9f
pranav s <dev.pranabsk@gmail.com>
2022-08-27 11:09:49 +0530

    Ultra Nation load counrries data using useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {countries.length}
+    <h1>All Countries Information</h1>
     </div>
   );
 }
